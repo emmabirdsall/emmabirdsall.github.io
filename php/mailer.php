3196dfb07adb1b296f0c3ba6d2fbe95386b6eff5
emmabirdsall <44851635+emmabirdsall@users.noreply.github.com>
2021-10-26 06:40:13 -0400

    10.26.2021 Updates 5
    Working on email contact form

--- php/mailer.php
+++ php/mailer.php
@@ -1,6 +1,6 @@
 <?php
 
-    if($_POST) {
+    if (isset($_POST["submit"])) {
         $to = "birdsallemma@gmail.com"; // your mail here
         $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
         $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
