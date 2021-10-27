a7ea461099ddfff2bd0324aa80a7017bed6d1336
emmabirdsall <44851635+emmabirdsall@users.noreply.github.com>
2021-10-27 06:40:09 -0400

    Form validation
    Add jQuery form validation plugin

--- js/script.js
+++ js/script.js
@@ -36,66 +36,27 @@ $(function() {
         
     });
 
-    //Contact Form function and validation
-    /*
-    var form = $('#form'),
-    email = $('#email'),
-    name = $('#name'),
-    subject = $('#subject'),
-    message = $('#message'),
-    info = $('#info'),
-    submit = $("#submit");
-
-    form.on('input', '#email, #name, #subject, #message', function() {
-        $(this).css('border-color', '');
-        info.html('').slideUp();
-    });
-    
-    submit.on('click', function(e) {
-        e.preventDefault();
-        if(validate()) {
-            $.ajax({
-            type: "POST",
-            url: "../php/mailer.php",
-            data: form.serialize(),
-            dataType: "json"
-        }).done(function(data) {
-            if(data.success) {
-            email.val('');
-            name.val('');
-            subject.val('');
-            message.val('');
-            info.html('Message sent!').css('color', 'green').slideDown();
-        } else {
-            info.html('Cannot send mail at this time, sorry! Please use the above email link instead.').css('color', 'red').slideDown();
+    //Contact Form validation using jQuery validation plugin
+    $("form[name='contact']").validate({
+        // Specify validation rules
+        rules: {
+          fullname: "required",
+          email: {
+            required: true,
+            email: true
+          },
+          subject: "required",
+          message: "required"
+        },
+        // Specify validation error messages
+        messages: {
+          firstname: "Please enter your full name",
+          email: "Please enter a valid email address",
+          subject: "Please enter a subject",
+          message: "Please enter a message"
+        },
+        submitHandler: function(form) {
+          form.submit();
         }
-        });
-        } 
-    });
-    
-    function validate() {
-        var valid = true;
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  
-        if(!regex.test(email.val())) {
-            email.css('border-color', 'red');
-            valid = false;
-        }
-        if($.trim(name.val()) === "") {
-            name.css('border-color', 'red');
-            valid = false;
-        }
-        if($.trim(subject.val()) === "") {
-            subject.css('border-color', 'red');
-            valid = false;
-        }
-        if($.trim(message.val()) === "") {
-            message.css('border-color', 'red');
-            valid = false;
-        }
-  
-        return valid;
-    }
-    */
-      
+      });
 });//Closure for jQuery initialization
